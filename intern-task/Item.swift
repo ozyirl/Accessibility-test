9255010b921da2c1758e1bf48abd6dfb5e5001f5
ozy <ozyftw17@gmail.com>
2024-04-11 22:06:09 +0530

    Initial Commit

---
+++ intern-task/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  intern-task
+//
+//  Created by ozy  on 11/04/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
